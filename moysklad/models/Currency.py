740d3c27209717dd465d1f6933451b629a8b05f4
DyshaGusew <pav060210@gmail.com>
2025-03-26 16:53:32 +1000

    Тест

--- moysklad/models/Currency.py
+++ moysklad/models/Currency.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Currency(models.Model):
-    meta_id = models.CharField(max_length=255, unique=True)
+    meta_id = models.CharField(max_length=254, unique=True)
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=3)
