c671381480190f3e935e0806716103a6b4d6398e
DyshaGusew <pav060210@gmail.com>
2025-04-07 03:40:45 +1000

    Настройка главной страницы

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from love_page.views import LoveDariaView
+from config.views import HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path("admin/", admin.site.urls),
     path("love-daria/", LoveDariaView.as_view(), name="love-daria"),
-    path("catalog/", include("catalog.urls")),
+    path("catalog/", include(("catalog.urls", "catalog"), namespace="catalog")),
 ]
