5a5d8a0af41d4ac5bba0b998e7a097c813c8521e
DyshaGusew <pav060210@gmail.com>
2025-04-08 14:33:13 +1000

    Создана страница о нас, исправлен footerЭ

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from love_page.views import LoveDariaView
-from config.views import HomeView
+from config.views import HomeView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path("admin/", admin.site.urls),
     path("love-daria/", LoveDariaView.as_view(), name="love-daria"),
     path("catalog/", include(("catalog.urls", "catalog"), namespace="catalog")),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- config/views.py
+++ config/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'config/index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'config/about.html'
